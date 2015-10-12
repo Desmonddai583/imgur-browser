cd1c0057ac0ef946e68a962b639335209059ebd7
Desmond dai <desmonddai583@gmail.com>
2015-10-12 18:52:21 +0800

    add topic and image

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,10 +4,12 @@ var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 
 var Main = require('./components/main');
+var Topic = require('./components/topic');
 
 module.exports = (
   <Router>
     <Route path="/" component={Main}>
+      <Route path="topics/:id" name="topics" component={Topic} />
     </Route>
   </Router>
 )
