a992f2980dcce3c90d3a8ae588430946410f0282
Desmond dai <desmonddai583@gmail.com>
2015-10-13 18:55:15 +0800

    project done

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,11 +5,13 @@ var Route = ReactRouter.Route;
 
 var Main = require('./components/main');
 var Topic = require('./components/topic');
+var ImageDetail = require('./components/image-detail');
 
 module.exports = (
   <Router>
     <Route path="/" component={Main}>
       <Route path="topics/:id" name="topics" component={Topic} />
+      <Route path="images/:id" name="images" component={ImageDetail} />
     </Route>
   </Router>
 )
